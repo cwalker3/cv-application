5567def5931043333a4b832d1b806293432960f8
Cory Walker <cory.walker.dev@gmail.com>
2023-09-13 14:38:54 -0700

    change component name

---
+++ src/components/Form.jsx
@@ -0,0 +1,11 @@
+function Form() {
+  return (
+    <form>
+      <input type="text" />
+      <input type="text" name="" id="" />
+      <input type="text" />
+    </form>
+  );
+}
+
+export default Form;
