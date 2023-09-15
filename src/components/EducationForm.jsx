812d0e5abd6aa13b61fd21f63679620850b0125f
Cory Walker <cory.walker.dev@gmail.com>
2023-09-15 16:32:20 -0700

    remove extra input

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -6,10 +6,6 @@ function EducationForm({data, onChange}) {
         <input type="text" id='school' value={data.school} onChange={onChange}/>
       </div>
       <div className="input-container">
-        <label htmlFor='school'>School</label>
-        <input type="text" id='school' value={data.school} onChange={onChange}/>
-      </div>
-      <div className="input-container">
         <label htmlFor='major'>Major</label>
         <input type="text" id='major' value={data.major} onChange={onChange}/>
       </div>
