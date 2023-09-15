25f5d50305621ddecd867890b9a408af85cdd3cf
Cory Walker <cory.walker.dev@gmail.com>
2023-09-15 15:48:22 -0700

    add data state

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+function App() {
+  const [data, setData] = useState({name: '', 
+    email: '', 
+    phone: '', 
+    school: '', 
+    major: '', 
+    schoolDate: '',
+    company: '',
+    position: '',
+    startDate: '',
+    endDate: ''
+  })
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
