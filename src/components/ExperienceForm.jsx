9d98154db96636db3bce6c16932f14d1a12a732a
Cory Walker <cory.walker.dev@gmail.com>
2023-09-15 16:36:13 -0700

    add ExperienceForm inputs

--- src/components/ExperienceForm.jsx
+++ src/components/ExperienceForm.jsx
@@ -1,7 +1,18 @@
 function ExperienceForm({data, onChange}) {
   return (
     <form>
-
+      <div className="input-container">
+        <label htmlFor='company'>Company</label>
+        <input type="text" id='school' value={data.company} onChange={onChange}/>
+      </div>
+      <div className="input-container">
+        <label htmlFor='position'>Position</label>
+        <input type="text" id='school' value={data.position} onChange={onChange}/>
+      </div>
+      <div className="input-container">
+        <label htmlFor='jobDate'>Dates Worked</label>
+        <input type="text" id='jobDate' value={data.jobDate} onChange={onChange}/>
+      </div>
     </form>
   )
 }
