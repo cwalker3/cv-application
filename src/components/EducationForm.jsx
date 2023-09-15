92df3adb960cc358411ef06f6e1516089c4dab4a
Cory Walker <cory.walker.dev@gmail.com>
2023-09-15 16:29:51 -0700

    Add form inputs

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -1,7 +1,22 @@
 function EducationForm({data, onChange}) {
   return (
     <form>
-
+      <div className="input-container">
+        <label htmlFor='school'>School</label>
+        <input type="text" id='school' value={data.school} onChange={onChange}/>
+      </div>
+      <div className="input-container">
+        <label htmlFor='school'>School</label>
+        <input type="text" id='school' value={data.school} onChange={onChange}/>
+      </div>
+      <div className="input-container">
+        <label htmlFor='major'>Major</label>
+        <input type="text" id='major' value={data.major} onChange={onChange}/>
+      </div>
+      <div className="input-container">
+        <label htmlFor='schoolDate'>Dates Attended</label>
+        <input type="text" id='schoolDate' value={data.schoolDate} onChange={onChange}/>
+      </div>
     </form>
   )
 }
