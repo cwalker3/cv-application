64daf206f6ab4be0b98dbb0d64a6f67ab0fc2fb7
Cory Walker <cory.walker.dev@gmail.com>
2023-09-15 16:24:23 -0700

    add labels

--- src/components/PersonalForm.jsx
+++ src/components/PersonalForm.jsx
@@ -2,15 +2,15 @@ function PersonalForm({data, onChange}) {
   return (
     <form>
       <div className="input-container">
-        <label htmlFor='name'></label>
+        <label htmlFor='name'>Full Name</label>
         <input type="text" id='name' value={data.name} onChange={onChange}/>
       </div>
       <div className="input-container">
-        <label htmlFor='email'></label>
+        <label htmlFor='email'>Email</label>
         <input type="email" id='email' value={data.email} onChange={onChange}/>
       </div>
       <div className="input-container">
-        <label htmlFor='phone'></label>
+        <label htmlFor='phone'>Phone Number</label>
         <input type="tel" id='phone' value={data.phone} onChange={onChange}/>
       </div>
     </form>
